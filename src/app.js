042eba841d5857d8d6c49c5c6537488a168fc20b
Kristian <kristian@coulsy.co.uk>
2018-04-02 12:37:04 +0100

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -13,15 +13,6 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-const visibleExpenses = getVisibleExpenses(store.getState().expenses,store.getState().filters);
-console.log(visibleExpenses);
-
-store.dispatch(addExpense({description: 'Water Bill', amount: 200, createdAt: 1001}));
-store.dispatch(addExpense({description: 'Gas Bill', amount: 2000, createdAt: 1002}));
-store.dispatch(addExpense({description: 'Rent', amount: 12000, createdAt: 500}));
-store.dispatch(addExpense({description: 'Internet', amount: 3000, createdAt: 2002}));
-
-
 
 const jsx = (
   <Provider store={store}>
@@ -29,6 +20,5 @@ const jsx = (
   </Provider>
 
 )
-
 
 ReactDOM.render(jsx, document.getElementById('app'));
