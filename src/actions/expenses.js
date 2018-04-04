2614d976d3fa6ffd100092c2fb5ed385687726e3
Kristian <kristian@coulsy.co.uk>
2018-04-04 14:49:38 +0100

    Setup test database environment

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -10,21 +10,19 @@ export const addExpense = (expense) => ({
 });
 export const startAddExpense = (expenseData = {}) =>{
   return (dispatch) => {
-      const {
-        description = '',
-        note = '',
-        amount = 0,
-        createdAt = 0
-      } = expenseData;
-      const expense = { description, note, amount, createdAt };
-      database.ref('expenses').push(expense).then((ref)=>{
-        dispatch(addExpense({
-          id: ref.key,
-          ...expense
-        }));
-      }).catch((e)=>{
-        
-      });
+    const {
+      description = '',
+      note = '',
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+    return database.ref('expenses').push(expense).then((ref)=>{
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }));
+    });
   };
 };
 //REMOVE_EXPENSE
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyCQrRURPqrvP9hsM1d9BjRoXJNALET_jNw",
-    authDomain: "expensify-ea737.firebaseapp.com",
-    databaseURL: "https://expensify-ea737.firebaseio.com",
-    projectId: "expensify-ea737",
-    storageBucket: "expensify-ea737.appspot.com",
-    messagingSenderId: "57426780438"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
